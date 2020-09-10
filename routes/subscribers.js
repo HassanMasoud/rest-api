2d9e8f59faa18d05d5d459a7142144dfbcc1ab74
Hassan Muhashamy <hassan.muhashamy@gmail.com>
2020-09-10 22:40:51 +0300

    Create create one route

--- routes/subscribers.js
+++ routes/subscribers.js
@@ -12,4 +12,18 @@ router.get("/", async (req, res) => {
   }
 });
 
+// Creating one
+router.post("/", async (req, res) => {
+  const subscriber = new Subscriber({
+    name: req.body.name,
+    subscribedToChannel: req.body.subscribedToChannel,
+  });
+  try {
+    const newSubscriber = await subscriber.save();
+    res.status(201).json(newSubscriber);
+  } catch (err) {
+    res.status(400).json({ message: err.message });
+  }
+});
+
 module.exports = router;
